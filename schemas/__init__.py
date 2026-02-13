cf49e53d72553ed4b3cb2eed49411d2bbe899eb4
Simon Ryu <simon.ryu@ibm.com>
2026-02-13 08:47:27 -0500

    Fix ModuleNotFoundError: removed imports of deleted schemas in schemas/__init__.py

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -3,11 +3,8 @@ Pydantic schemas for type-safe data transfer.
 """
 
 from schemas.user import UserSchema, UserCreateSchema, UserUpdateSchema
-from schemas.profile import ProfileFactSchema, ProfileFactCreateSchema, ProfileSchema
 from schemas.conversation import ConversationSchema, ConversationCreateSchema
-from schemas.timeline import TimelineEventSchema, TimelineEventCreateSchema
 from schemas.diary import DiaryEntrySchema, DiaryEntryCreateSchema
-from schemas.scheduled_message import ScheduledMessageSchema, ScheduledMessageCreateSchema
 from schemas.context import UserContextSchema
 from schemas.token_usage import TokenUsageSchema, TokenUsageCreateSchema
 
@@ -15,17 +12,10 @@ __all__ = [
     "UserSchema",
     "UserCreateSchema",
     "UserUpdateSchema",
-    "ProfileFactSchema",
-    "ProfileFactCreateSchema",
-    "ProfileSchema",
     "ConversationSchema",
     "ConversationCreateSchema",
-    "TimelineEventSchema",
-    "TimelineEventCreateSchema",
     "DiaryEntrySchema",
     "DiaryEntryCreateSchema",
-    "ScheduledMessageSchema",
-    "ScheduledMessageCreateSchema",
     "UserContextSchema",
     "TokenUsageSchema",
     "TokenUsageCreateSchema",
